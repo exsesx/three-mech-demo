7faf8c16998730fe0a08b3da1827a43d5a3c9bd0
Oleg Vanin <ovanin@s-pro.io>
2022-04-06 17:34:39 +0300

    feat: add settings UI; enable SWC minification; add loader

--- components/MainScene.js
+++ components/MainScene.js
@@ -3,22 +3,34 @@ import { OrbitControls, Loader } from "@react-three/drei";
 import { Canvas } from "@react-three/fiber";
 import { Vector3 } from "three";
 import { Suspense } from "react";
+import { Center } from "@mantine/core";
+import { useControls, Leva } from "leva";
 
-export default function Home() {
+export default function MainScene(props) {
+  const { enabled, intensity } = useControls("Lights", {
+    enabled: true,
+    intensity: 1,
+  });
+  const { autoRotate, autoRotateSpeed } = useControls("Controls", {
+    autoRotate: true,
+    autoRotateSpeed: 3,
+  });
+
   return (
-    <>
-      <Canvas camera={{ position: [0, 100, 200] }}>
+    <Center style={{ height: "100%", width: "100%", zIndex: 2 }}>
+      <Leva collapsed />
+      <Canvas camera={{ position: [0, 100, 200] }} onCreated={props.onCreated}>
         <Suspense fallback={null}>
-          <ambientLight intensity={1} />
+          {enabled && <ambientLight intensity={intensity} />}
           <OrbitControls
-            autoRotate
-            autoRotateSpeed={3}
+            autoRotate={autoRotate}
+            autoRotateSpeed={autoRotateSpeed}
             target={new Vector3(0, 50, 0)}
           />
           <MechModel />
         </Suspense>
       </Canvas>
       <Loader />
-    </>
+    </Center>
   );
 }
--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,35 @@
 import dynamic from "next/dynamic";
+import { Center, Loader } from "@mantine/core";
+import { useState } from "react";
 
 const MainScene = dynamic(() => import("../components/MainScene"), {
   ssr: false,
 });
 
 export default function Home() {
-  return <MainScene />;
+  const [created, setCreated] = useState(false);
+
+  return (
+    <>
+      {!created && (
+        <Center
+          style={{
+            height: "100%",
+            width: "100%",
+            zIndex: 1,
+            position: "absolute",
+            top: 0,
+          }}
+        >
+          <Loader />
+        </Center>
+      )}
+      <MainScene
+        onCreated={(state) => {
+          console.log("--> Canvas created", state);
+          setCreated(true);
+        }}
+      />
+    </>
+  );
 }
